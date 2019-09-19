42f79ac2797d2f3a00d692316ae994b3be35b672
Emily Harber <emily.a.harber@gmail.com>
2019-09-19 17:15:11 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,10 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts["Freddy Mercury"].each do |attribute, value|
+    if attribute == :favorite_ice_cream_flavors 
+      value.delete_if {|ice_cream| ice_cream == "strawberry"}
+    end 
+  end 
+  contacts 
 end
